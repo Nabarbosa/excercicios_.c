0f64cf8141ed0fc78383cb48ab524c0e80212d2d
Nabarbosa <160445609+Nabarbosa@users.noreply.github.com>
2024-03-12 09:13:41 -0300

    Add files via upload

---
+++ 3notasmediaaprovadooureprovado.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	int nota, media, i;
+	float mediaFinal;
+	
+	for(i = 1; i <= 3; i++) {
+		printf("Digite a sua %iª nota: ", i);
+		scanf("%i",&nota);
+		
+		media += nota;
+	}
+	
+	mediaFinal = media / 3;
+	
+	printf("\nMédia final: %2.f", mediaFinal);
+	
+	if(mediaFinal >= 7){
+		printf("\n === Situação : Aprovado ===");
+	} if(mediaFinal <= 4){
+		printf("\n === Situação : Reprovado ===");
+	}
+	
+	return 0;
+}
