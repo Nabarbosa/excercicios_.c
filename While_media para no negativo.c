e4c60fdbb16398e3cca3fb10b7bc56bc690a6de0
Nabarbosa <160445609+Nabarbosa@users.noreply.github.com>
2024-03-14 09:05:02 -0300

    Update While_media para no negativo.c

--- While_media para no negativo.c
+++ While_media para no negativo.c
@@ -7,22 +7,23 @@ int main() {
 	int numero, soma = 0, contador = 0;
 	float media;
 	
-	printf("Digite um número: ");
+	printf("Digite um nÃºmero: ");
 	scanf("%i",&numero);
 	
 	while(numero > 0) {
 		contador++;
 		soma += numero;
 		
-	printf("Digite um número: ");
+	printf("Digite um nÃºmero: ");
 	scanf("%i",&numero);
 	}
 	
 	if (contador == 0) {
-		printf("Não foi informado um número positivo!");
+		printf("NÃ£o foi informado um nÃºmero positivo!");
 	} else {
+		//cast
 		media = soma / (float) contador;
-		printf("Média: %.1f \n", media);
+		printf("MÃ©dia: %.1f \n", media);
 	}
 	
 	return 0;
