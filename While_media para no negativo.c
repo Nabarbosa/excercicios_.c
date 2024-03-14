d4e6f9bdbb01b5205b96de08279381c39e844bf2
Nabarbosa <160445609+Nabarbosa@users.noreply.github.com>
2024-03-14 09:04:00 -0300

    Add files via upload

---
+++ While_media para no negativo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL,"");
+	
+	int numero, soma = 0, contador = 0;
+	float media;
+	
+	printf("Digite um número: ");
+	scanf("%i",&numero);
+	
+	while(numero > 0) {
+		contador++;
+		soma += numero;
+		
+	printf("Digite um número: ");
+	scanf("%i",&numero);
+	}
+	
+	if (contador == 0) {
+		printf("Não foi informado um número positivo!");
+	} else {
+		media = soma / (float) contador;
+		printf("Média: %.1f \n", media);
+	}
+	
+	return 0;
+}
